1d0d7a6207938c6263c3dfa0281c47bff4094922
mburdett <michael_burdett@ajg.com>
2023-12-08 13:29:43 +0000

    Added new components and data files to app module
    Updated the main application module file with the import statements for Day01, Day02, Day03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Day01Component} from "./day01/day01.component";
+import {Day02Component} from "./day02/day02.component";
+import {HomeComponent} from "./home/home.component";
+import {Day03Component} from "./day03/day03.component";
+import {Day04Component} from "./day04/day04.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'day01',
+    component: Day01Component
+  },
+  {
+    path: 'day02',
+    component: Day02Component
+  },
+  {
+    path: 'day03',
+    component: Day03Component
+  },
+  {
+    path: 'day04',
+    component: Day04Component
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Day01Component } from './day01/day01.component';
+import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
+import { Day02Component } from './day02/day02.component';
+import { HomeComponent } from './home/home.component';
+import { Day03Component } from './day03/day03.component';
+import { Day04Component } from './day04/day04.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Day01Component,
+    Day02Component,
+    HomeComponent,
+    Day03Component,
+    Day04Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
